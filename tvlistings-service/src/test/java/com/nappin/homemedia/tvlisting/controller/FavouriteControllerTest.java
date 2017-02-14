3dac6c56511c96284f2fc91e08856480ad7a9c30
chrisnappin <chris@nappin.com>
2017-02-14 16:23:57 +0000

    Refactored higher level common REST controller tests.

--- tvlistings-service/src/test/java/com/nappin/homemedia/tvlisting/controller/FavouriteControllerTest.java
+++ tvlistings-service/src/test/java/com/nappin/homemedia/tvlisting/controller/FavouriteControllerTest.java
@@ -1,5 +1,7 @@
 package com.nappin.homemedia.tvlisting.controller;
 
+import com.nappin.homemedia.tvlisting.test.RESTControllerTester;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,22 +24,25 @@ public class FavouriteControllerTest {
     @Autowired
     private MockMvc mvc;
 
+    private RESTControllerTester tester;
+
+    @Before
+    public void setUp() {
+        this.tester = new RESTControllerTester(mvc);
+    }
+
     /**
      * Tests the <code>getFavouriteChannels</code> method, when successful.
      * @throws Exception    Test failed
      */
     @Test
     public void getFavouriteChannelsSuccessful() throws Exception {
-        String expectedResponse =
+        tester.isSuccessful(mvc,
+                get("/favourite/channel/{userId}", 1234L),
                 "[" +
-                        "{\"name\":\"Example Channel #1\",\"id\":100}," +
-                        "{\"name\":\"Example Channel #2\",\"id\":101}" +
-                "]";
-
-        mvc.perform(get("/favourite/channel/{userId}", 1234L).accept(MediaType.APPLICATION_JSON_UTF8))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-                .andExpect(content().string(expectedResponse));
+                            "{\"name\":\"Example Channel #1\",\"id\":100}," +
+                            "{\"name\":\"Example Channel #2\",\"id\":101}" +
+                        "]");
     }
 
     /**
@@ -46,12 +51,9 @@ public class FavouriteControllerTest {
      */
     @Test
     public void getFavouriteChannelsEmpty() throws Exception {
-        String expectedResponse = "[]";
-
-        mvc.perform(get("/favourite/channel/{userId}", 1235L).accept(MediaType.APPLICATION_JSON_UTF8))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-                .andExpect(content().string(expectedResponse));
+        tester.isSuccessful(mvc,
+                get("/favourite/channel/{userId}", 1235L),
+                "[]");
     }
 
     /**
@@ -60,12 +62,9 @@ public class FavouriteControllerTest {
      */
     @Test
     public void getFavouriteChannelsUserUnknown() throws Exception {
-        String expectedResponse = "{\"message\":\"User 1236 is unknown\"}";
-
-        mvc.perform(get("/favourite/channel/{userId}", 1236L).accept(MediaType.APPLICATION_JSON_UTF8))
-                .andExpect(status().isNotFound())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-                .andExpect(content().string(expectedResponse));
+        tester.isNotFound(mvc,
+                get("/favourite/channel/{userId}", 1236L),
+                "{\"message\":\"User 1236 is unknown\"}");
     }
 
     /**
@@ -74,11 +73,7 @@ public class FavouriteControllerTest {
      */
     @Test
     public void getFavouriteChannelsMissingArgument() throws Exception {
-        String expectedResponse = ""; // no response body
-
-        mvc.perform(get("/favourite/channel").accept(MediaType.APPLICATION_JSON_UTF8))
-                .andExpect(status().isNotFound())
-                .andExpect(content().string(expectedResponse));
+        tester.isNotFoundEmpty(mvc, get("/favourite/channel"));
     }
 
     /**
@@ -87,11 +82,7 @@ public class FavouriteControllerTest {
      */
     @Test
     public void getFavouriteChannelsInvalidArgument() throws Exception {
-        String expectedResponse = ""; // no response body
-
-        mvc.perform(get("/favourite/channel/{userId}", "wibble").accept(MediaType.APPLICATION_JSON_UTF8))
-                .andExpect(status().isBadRequest())
-                .andExpect(content().string(expectedResponse));
+        tester.isBadRequestEmpty(mvc, get("/favourite/channel/{userId}", "wibble"));
     }
 
     /**
@@ -101,16 +92,11 @@ public class FavouriteControllerTest {
     @Test
     public void getFavouriteChannelsMissingAccept() throws Exception {
         // request with missing accept media type still returns a valid response
-        String expectedResponse =
+        tester.isSuccessfulNoAccept(mvc, get("/favourite/channel/{userId}", 1234L),
                 "[" +
-                        "{\"name\":\"Example Channel #1\",\"id\":100}," +
-                        "{\"name\":\"Example Channel #2\",\"id\":101}" +
-                "]";
-
-        mvc.perform(get("/favourite/channel/{userId}", 1234L))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-                .andExpect(content().string(expectedResponse));
+                            "{\"name\":\"Example Channel #1\",\"id\":100}," +
+                            "{\"name\":\"Example Channel #2\",\"id\":101}" +
+                        "]");
     }
 
     /**
@@ -119,16 +105,10 @@ public class FavouriteControllerTest {
      */
     @Test
     public void getFavouriteChannelsWrongAccept() throws Exception {
-        String expectedResponse = ""; // no response body
-
-        mvc.perform(get("/favourite/channel/{userId}", "1234").accept(MediaType.APPLICATION_XML))
-                .andExpect(status().isNotAcceptable())
-                .andExpect(content().string(expectedResponse));
+        tester.isNotAcceptableEmpty(mvc, get("/favourite/channel/{userId}", "1234"));
     }
 
     // TODO: internal server error
 
     // TODO: all tests for addFavouriteChannel, removeFavouriteChannel
-
-    // TODO: refactor common test so this uses less code - extract common utils perhaps?
 }
---
+++ tvlistings-service/src/test/java/com/nappin/homemedia/tvlisting/test/RESTControllerTester.java
@@ -0,0 +1,103 @@
+package com.nappin.homemedia.tvlisting.test;
+
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * Utility used to simplify testing Spring REST controllers in a consistent manner.
+ */
+public class RESTControllerTester {
+
+    private final MockMvc mvc;
+
+    public RESTControllerTester(MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    /**
+     * Tests that the specified request was successful (200), and contained the expected JSON result.
+     * @param mvc               The MockMvc to use
+     * @param requestBuilder    The request to use
+     * @param expectedResponse  The expected response to check for
+     * @throws Exception    Test failed
+     */
+    public void isSuccessful(MockMvc mvc, MockHttpServletRequestBuilder requestBuilder,
+                                         String expectedResponse) throws Exception {
+        mvc.perform(requestBuilder.accept(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(content().string(expectedResponse));
+    }
+
+    /**
+     * Tests that the specified request (with no JSON accept header) was successful (200), and contained the
+     * expected JSON result.
+     * @param mvc               The MockMvc to use
+     * @param requestBuilder    The request to use
+     * @param expectedResponse  The expected response to check for
+     * @throws Exception    Test failed
+     */
+    public void isSuccessfulNoAccept(MockMvc mvc, MockHttpServletRequestBuilder requestBuilder,
+                             String expectedResponse) throws Exception {
+        mvc.perform(requestBuilder)
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(content().string(expectedResponse));
+    }
+
+    /**
+     * Tests that the specified request was not found (404), and contained the expected JSON result.
+     * @param mvc               The MockMvc to use
+     * @param requestBuilder    The request to use
+     * @param expectedResponse  The expected response to check for
+     * @throws Exception    Test failed
+     */
+    public void isNotFound(MockMvc mvc, MockHttpServletRequestBuilder requestBuilder,
+                             String expectedResponse) throws Exception {
+        mvc.perform(requestBuilder.accept(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(status().isNotFound())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(content().string(expectedResponse));
+    }
+
+    /**
+     * Tests that the specified request was not found (404), and did not contain any result.
+     * @param mvc               The MockMvc to use
+     * @param requestBuilder    The request to use
+     * @throws Exception    Test failed
+     */
+    public void isNotFoundEmpty(MockMvc mvc, MockHttpServletRequestBuilder requestBuilder) throws Exception {
+        mvc.perform(requestBuilder)
+                .andExpect(status().isNotFound())
+                .andExpect(content().string(""));
+    }
+
+    /**
+     * Tests that the specified request was a bad request (400), and did not contain any result.
+     * @param mvc               The MockMvc to use
+     * @param requestBuilder    The request to use
+     * @throws Exception    Test failed
+     */
+    public void isBadRequestEmpty(MockMvc mvc, MockHttpServletRequestBuilder requestBuilder) throws Exception {
+        mvc.perform(requestBuilder)
+                .andExpect(status().isBadRequest())
+                .andExpect(content().string(""));
+    }
+
+        /**
+         * Tests that the specified request (with an XML accept header) was not acceptable (406), and did not
+         * contain any result.
+         * @param mvc               The MockMvc to use
+         * @param requestBuilder    The request to use
+         * @throws Exception    Test failed
+         */
+    public void isNotAcceptableEmpty(MockMvc mvc, MockHttpServletRequestBuilder requestBuilder) throws Exception {
+        mvc.perform(requestBuilder.accept(MediaType.APPLICATION_XML))
+                .andExpect(status().isNotAcceptable())
+                .andExpect(content().string(""));
+    }
+}
