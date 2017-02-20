4328532af050a0891c67c167bc1b6f5f5eb26f4d
chrisnappin <chris@nappin.com>
2017-02-20 17:07:30 +0000

    Added springfox (swagger docs).

--- tvlistings-service/src/main/java/com/nappin/homemedia/tvlisting/configuration/ApplicationConfiguration.java
+++ tvlistings-service/src/main/java/com/nappin/homemedia/tvlisting/configuration/ApplicationConfiguration.java
@@ -4,11 +4,13 @@ import com.nappin.homemedia.tvlisting.service.FavouriteService;
 import com.nappin.homemedia.tvlisting.service.impl.FavouriteServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * The Spring application configuration.
  */
 @Configuration
+@EnableSwagger2
 public class ApplicationConfiguration {
 
     @Bean
